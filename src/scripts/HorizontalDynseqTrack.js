381f94d715716eea1c31769cfc7ce713cafbdfca
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2022-02-10 22:00:54 -0800

    Fix the height of the characters where they were previously cut off

--- src/scripts/HorizontalDynseqTrack.js
+++ src/scripts/HorizontalDynseqTrack.js
@@ -86,8 +86,8 @@ export default function HDT(HGC, ...args) {
       }
 
       const minimumsPerTile = visibleAndFetchedIds
-        .map(x => this.makeDataOnlyTile(this.fetchedTiles[x]))
-        .map(tile => {
+        .map((x) => this.makeDataOnlyTile(this.fetchedTiles[x]))
+        .map((tile) => {
           const ind = this.getIndicesOfVisibleDataInTile(tile);
           return tile.tileData.denseDataExtrema.getMinNonZeroInSubset(ind);
         });
@@ -110,8 +110,8 @@ export default function HDT(HGC, ...args) {
       }
 
       const maximumsPerTile = visibleAndFetchedIds
-        .map(x => this.makeDataOnlyTile(this.fetchedTiles[x]))
-        .map(tile => {
+        .map((x) => this.makeDataOnlyTile(this.fetchedTiles[x]))
+        .map((tile) => {
           const ind = this.getIndicesOfVisibleDataInTile(tile);
           return tile.tileData.denseDataExtrema.getMaxNonZeroInSubset(ind);
         });
@@ -141,7 +141,7 @@ export default function HDT(HGC, ...args) {
       this.fontColors = fontColors;
 
       this.maxCharWidth = 0;
-      this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(char => {
+      this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((char) => {
         if (newOptions.nonStandardSequence || 'ATCGN'.includes(char)) {
           const fill = HGC.utils.colorToHex(
             fontColors[char] || newOptions.defaultFontColor || '#ffb347',
@@ -214,7 +214,12 @@ export default function HDT(HGC, ...args) {
         .range([tileX, tileX + tileWidth]);
       let width = (this._xScale(tileX + tileWidth) - this._xScale(tileX)) / dlen;
 
-      const middle = this.valueScale(offsetValue);
+      if (this.valueScale.domain()[0] > 0) {
+        // make sure to always extend the scale down to 0
+        this.valueScale = this.valueScale.domain([0, this.valueScale.domain()[1]]);
+      }
+
+      const middle = this.valueScale.range()[0];
       const maxFontSize = this.options.maxFontSize || this.dimensions[1] / 2;
       const minFontSize = this.options.minFontSize || 1.5;
       const fadeOutFontSize = Math.max(this.options.fadeOutFontSize || 5, minFontSize + 0.01);
@@ -258,6 +263,7 @@ export default function HDT(HGC, ...args) {
             // Data is in the last tile and extends beyond the coordinate system.
             break;
           }
+
           // No need to render anything for 0 or NaN
           if (!dataValue) {
             continue;
@@ -326,7 +332,7 @@ export default function HDT(HGC, ...args) {
 
         // At most 2048 characters on screen
         const shouldFetchFasta = maxZoom - this.zoomLevel < 2;
-        this.dataFetcher.setFilter(_ => shouldFetchFasta, 1);
+        this.dataFetcher.setFilter((_) => shouldFetchFasta, 1);
       }
       super.refreshTiles();
     }
@@ -404,7 +410,7 @@ export default function HDT(HGC, ...args) {
       const strokeColor = this.options.lineStrokeColor ? this.options.lineStrokeColor : 'blue';
       const strokeWidth = this.options.lineStrokeWidth ? this.options.lineStrokeWidth : 1;
 
-      this.visibleAndFetchedTiles().forEach(tile => {
+      this.visibleAndFetchedTiles().forEach((tile) => {
         // First we'll draw the line element.
         const path = document.createElement('path');
         path.setAttribute('d', tile.path);
@@ -447,7 +453,7 @@ export default function HDT(HGC, ...args) {
 const svgIcon =
   '<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5"><path d="M4 2.1L.5 3.5v12l5-2 5 2 5-2v-12l-5 2-3.17-1.268" fill="none" stroke="currentColor"/><path d="M10.5 3.5v12" fill="none" stroke="currentColor" stroke-opacity=".33" stroke-dasharray="1,2,0,0"/><path d="M5.5 13.5V6" fill="none" stroke="currentColor" stroke-opacity=".33" stroke-width=".9969299999999999" stroke-dasharray="1.71,3.43,0,0"/><path d="M9.03 5l.053.003.054.006.054.008.054.012.052.015.052.017.05.02.05.024 4 2 .048.026.048.03.046.03.044.034.042.037.04.04.037.04.036.042.032.045.03.047.028.048.025.05.022.05.02.053.016.053.014.055.01.055.007.055.005.055v.056l-.002.056-.005.055-.008.055-.01.055-.015.054-.017.054-.02.052-.023.05-.026.05-.028.048-.03.046-.035.044-.035.043-.038.04-4 4-.04.037-.04.036-.044.032-.045.03-.046.03-.048.024-.05.023-.05.02-.052.016-.052.015-.053.012-.054.01-.054.005-.055.003H8.97l-.053-.003-.054-.006-.054-.008-.054-.012-.052-.015-.052-.017-.05-.02-.05-.024-4-2-.048-.026-.048-.03-.046-.03-.044-.034-.042-.037-.04-.04-.037-.04-.036-.042-.032-.045-.03-.047-.028-.048-.025-.05-.022-.05-.02-.053-.016-.053-.014-.055-.01-.055-.007-.055L4 10.05v-.056l.002-.056.005-.055.008-.055.01-.055.015-.054.017-.054.02-.052.023-.05.026-.05.028-.048.03-.046.035-.044.035-.043.038-.04 4-4 .04-.037.04-.036.044-.032.045-.03.046-.03.048-.024.05-.023.05-.02.052-.016.052-.015.053-.012.054-.01.054-.005L8.976 5h.054zM5 10l4 2 4-4-4-2-4 4z" fill="currentColor"/><path d="M7.124 0C7.884 0 8.5.616 8.5 1.376v3.748c0 .76-.616 1.376-1.376 1.376H3.876c-.76 0-1.376-.616-1.376-1.376V1.376C2.5.616 3.116 0 3.876 0h3.248zm.56 5.295L5.965 1H5.05L3.375 5.295h.92l.354-.976h1.716l.375.975h.945zm-1.596-1.7l-.592-1.593-.58 1.594h1.172z" fill="currentColor"/></svg>';
 
-const pxSizes = sizes => {
+const pxSizes = (sizes) => {
   const options = {};
   for (const size of sizes) {
     options[size] = { name: size + 'px', value: size };
